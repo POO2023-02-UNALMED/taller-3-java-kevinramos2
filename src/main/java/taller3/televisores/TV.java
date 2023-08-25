89a42b0da5a2123da7829ecd1a0ed632356cdba3
kevinramos2 <keramosl@unal.edu.co>
2023-08-25 17:53:34 -0500

    Update TV.java
    test canal

--- src/main/java/taller3/televisores/TV.java
+++ src/main/java/taller3/televisores/TV.java
@@ -70,13 +70,13 @@ public class TV {
 	}
 	public void canalUp () {
 		//condiciones para subir de canal
-		if ((canal != 120) && (estado)) {
+		if ((canal >= 1 && canal < 121) && (estado)) {
 			canal = canal+1;
 		}
 	}
 	public void canalDown() {
 		//condiciones para bajar de canal
-		if ((canal != 1) && (estado)) {
+		if ((canal > 0) && (estado)) {
 			canal = canal-1;
 		}
 	}
