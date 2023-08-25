5c7c7eef59dd36faa0280c8c1fcb355a85332cdb
kevinramos2 <keramosl@unal.edu.co>
2023-08-25 16:07:10 -0500

    Segundo
    creación del atributo y sus métodos

--- src/main/java/taller3/televisores/Marca.java
+++ src/main/java/taller3/televisores/Marca.java
@@ -1,5 +1,18 @@
 package taller3.televisores;
 
 public class Marca {
-
+	//atributo string
+	private String nombre;
+	//constructor con el nombre del objeto Marca
+	public Marca(String nombre){
+		this.nombre = nombre;
+	}
+	//método get
+	public String getNombre() {
+		return this.nombre;
+	}
+	//método set
+	public void setNombre () {
+		this.nombre = nombre;
+	}
 }
