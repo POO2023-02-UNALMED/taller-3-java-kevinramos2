cd0ea6b4036bdfca86f9060a8a38af11f8a657c5
kevinramos2 <keramosl@unal.edu.co>
2023-08-25 17:34:02 -0500

    control
    control "total"

--- src/main/java/taller3/televisores/Control.java
+++ src/main/java/taller3/televisores/Control.java
@@ -10,6 +10,37 @@ public class Control {
 		this.tv = tv;
 	}
 	
-
-
+	public void turnOn() {
+		tv.turnOn();
+	}
+	public void turnOff() {
+		tv.turnOff();
+	}
+	
+	public void canalDown() {
+		tv.canalDown();
+	}
+	public void canalUp() {
+		tv.canalUp();
+	}
+	
+	public void volumenDown() {
+		tv.volumenDown();
+	}
+	public void volumenUp() {
+		tv.volumenUp();
+	}
+	
+	public void setVolumen(int volumen) {
+		tv.setVolumen(volumen);
+	}
+	
+	public void setCanal(int canal) {
+		tv.setCanal(canal);
+	}
+	
+	public void enlazar(TV televisor) {
+		this.tv = televisor;
+		televisor.setControl(this);
+	}
 }
